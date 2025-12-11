87d51c0fd93092d7313c38c9c3a916f9bb7b3ff1
Happid <happidilmi@gmail.com>
2025-12-11 17:26:15 +0700

    remove unused directory

--- 5-this-binding/1-global.js
+++
@@ -1,9 +0,0 @@
-console.log('1. this di global scope ...');
-
-console.log(this); // Window { ... }
-
-var a = 10;
-console.log("Variable Declaration (var) => ", this.a); // 10
-
-let b = 20;
-console.log("Variable Declaration (let) => ", this.b); // undefined
--- 5-this-binding/README.md
+++
@@ -1,6 +0,0 @@
-### Perilaku this berbeda tergantung:
-
-1. Global scope
-2. Object method 
-3. Class 
-4. Arrow function
